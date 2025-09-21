48eb722e5d095cebd46f64e7692a65c146d9769c
Martinatjie <martinabrockmann@gmail.com>
2025-09-21 17:03:15 +0200

    Add project files.

---
+++ ExpenseAgent/Program.cs
@@ -0,0 +1,7 @@
+using ExpenseAgent;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
